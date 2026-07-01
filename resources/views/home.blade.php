9b4f7b72bc65aad804b998f2325badcb03ee0ea0
Test User <test@test.com>
2026-07-01 14:39:54 +0300

    Remove Haftanın Favorileri section from homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,7 @@
 <x-layouts.app>
     @include('livewire.home.hero-section')
     
-    <div x-data="{ shown: false }" x-intersect.once="shown = true" :class="shown ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'" class="transition-all duration-1000 ease-out">
-        <livewire:product.best-seller-carousel />
-    </div>
+
     
     <div x-data="{ shown: false }" x-intersect.once="shown = true" :class="shown ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'" class="pt-4 pb-16 bg-white transition-all duration-1000 ease-out">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
