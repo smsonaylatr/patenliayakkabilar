a7912ab74fb49d0a387ad36e5ac368d0bc7f9709
Test User <test@test.com>
2026-07-12 03:37:55 +0300

    UI Düzeltmesi: Yorumlar modülü, tam olarak ana görselin sağ ve sol hizasıyla birebir örtüşecek (küçük resimlerin altında boşluk bırakılacak ve sağda hiç ekstra boşluk kalmayacak) şekilde son haline getirildi.

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -287,7 +287,14 @@
                 
                 <!-- 3. Product Reviews (Left Column, Bottom on Desktop | Bottom on Mobile) -->
                 <div class="order-3 lg:col-span-1 lg:row-span-1 mt-6 px-4 sm:px-0">
-                    @include('livewire.product.review-list', ['product' => $product])
+                    <div class="flex flex-col md:flex-row gap-4 lg:gap-6">
+                        <!-- Spacer to align with thumbnails -->
+                        <div class="hidden md:block w-full md:w-24 lg:w-28 flex-shrink-0"></div>
+                        <!-- Reviews Container aligned with main image -->
+                        <div class="flex-1">
+                            @include('livewire.product.review-list', ['product' => $product])
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
