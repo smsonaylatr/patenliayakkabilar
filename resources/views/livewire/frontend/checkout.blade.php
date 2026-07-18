134ef24e9171d237ce4735161c93bd0b875c63d6
Test User <test@test.com>
2026-07-18 18:51:00 +0300

    style(checkout): constrain paytr iframe width for a compact and minimalist design

--- resources/views/livewire/frontend/checkout.blade.php
+++ resources/views/livewire/frontend/checkout.blade.php
@@ -11,8 +11,8 @@
             <!-- Sol Taraf: Form -->
             <div class="flex-1">
                 @if($paytr_token)
-                    <div class="bg-white p-0 sm:p-6 rounded-2xl shadow-sm sm:border sm:border-gray-100">
-                        
+                    <!-- PayTR formunun devasa uzamasını engellemek için max-w-[500px] eklendi -->
+                    <div class="bg-white p-0 sm:p-6 rounded-2xl shadow-sm sm:border sm:border-gray-100 max-w-[500px]">
                         <!-- PayTR Iframe -->
                         <iframe x-data x-init="$nextTick(() => { if(typeof iFrameResize !== 'undefined') { iFrameResize({}, $el); } })" src="https://www.paytr.com/odeme/guvenli/{{ $paytr_token }}" id="paytriframe" frameborder="0" scrolling="no" style="width: 100%; min-height: 400px;"></iframe>
                     </div>
