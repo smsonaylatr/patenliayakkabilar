5689297a82c1cfff2422399b958aae399f91eecb
Test User <test@test.com>
2026-07-22 18:51:19 +0300

    style: revert reviews container width back to max-w-4xl

--- resources/views/products/reviews.blade.php
+++ resources/views/products/reviews.blade.php
@@ -2,7 +2,7 @@
     <x-slot:title>{{ $product->name }} Yorumları | Patenli Ayakkabılar</x-slot:title>
 
     <div class="pt-4 lg:pt-16 pb-20 bg-gray-50 min-h-screen">
-        <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
             {{-- Breadcrumb --}}
             <div class="mb-8">
                 <x-breadcrumb :items="[
