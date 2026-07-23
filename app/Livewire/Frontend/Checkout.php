ef5c9130ded2c48cd3bc38bb1761b4c12b2dadd4
Test User <test@test.com>
2026-07-23 14:34:57 +0300

    fix: force PayTR iframe to load in light mode

--- app/Livewire/Frontend/Checkout.php
+++ app/Livewire/Frontend/Checkout.php
@@ -260,7 +260,8 @@ class Checkout extends Component
                 'paytr_token' => $paytr_token,
                 'debug_on' => 1,
                 'timeout_limit' => 30,
-                'test_mode' => $test_mode
+                'test_mode' => $test_mode,
+                'iframe_v2_dark' => 0
             ];
         } else {
             $hash_str = $merchant_id .$user_ip .$merchant_oid .$email .$payment_amount .$user_basket .$no_installment .$max_installment .$currency .$test_mode;
@@ -284,7 +285,8 @@ class Checkout extends Component
                 'merchant_fail_url' => route('order.fail', ['order_number' => $order->order_number]),
                 'timeout_limit' => 30,
                 'currency' => $currency,
-                'test_mode' => $test_mode
+                'test_mode' => $test_mode,
+                'iframe_v2_dark' => 0
             ];
         }
 
