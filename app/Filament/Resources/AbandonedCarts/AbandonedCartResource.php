25b495abd23702555b5fa0a7212a467c829e4e7f
Test User <test@test.com>
2026-07-23 18:22:25 +0300

    fix: correct Section namespace in AbandonedCartResource

--- app/Filament/Resources/AbandonedCarts/AbandonedCartResource.php
+++ app/Filament/Resources/AbandonedCarts/AbandonedCartResource.php
@@ -7,7 +7,7 @@ use App\Filament\Resources\AbandonedCarts\Tables\AbandonedCartsTable;
 use App\Models\Cart;
 use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
