b26ecf5ebb6fa3c2aa5234fda815a168388775cc
Test User <test@test.com>
2026-07-23 15:21:36 +0300

    fix: force light color scheme on PayTR iframe via CSS

--- resources/views/livewire/frontend/checkout.blade.php
+++ resources/views/livewire/frontend/checkout.blade.php
@@ -29,12 +29,12 @@
                         @if($payment_method === 'wire_transfer')
                             <div wire:poll.3s="checkOrderStatus">
                                 <div wire:ignore>
-                                    <iframe x-data x-init="$nextTick(() => { if(typeof iFrameResize !== 'undefined') { iFrameResize({ checkOrigin: false }, $el); } })" src="https://www.paytr.com/odeme/api/{{ $paytr_token }}" id="paytriframe" frameborder="0" scrolling="no" style="width: 100%; min-height: 400px;"></iframe>
+                                    <iframe x-data x-init="$nextTick(() => { if(typeof iFrameResize !== 'undefined') { iFrameResize({ checkOrigin: false }, $el); } })" src="https://www.paytr.com/odeme/api/{{ $paytr_token }}" id="paytriframe" frameborder="0" scrolling="no" style="width: 100%; min-height: 400px; color-scheme: light;"></iframe>
                                 </div>
                             </div>
                         @else
                             <div wire:ignore>
-                                <iframe x-data x-init="$nextTick(() => { if(typeof iFrameResize !== 'undefined') { iFrameResize({ checkOrigin: false }, $el); } })" src="https://www.paytr.com/odeme/guvenli/{{ $paytr_token }}" id="paytriframe" frameborder="0" scrolling="no" style="width: 100%; min-height: 400px;"></iframe>
+                                <iframe x-data x-init="$nextTick(() => { if(typeof iFrameResize !== 'undefined') { iFrameResize({ checkOrigin: false }, $el); } })" src="https://www.paytr.com/odeme/guvenli/{{ $paytr_token }}" id="paytriframe" frameborder="0" scrolling="no" style="width: 100%; min-height: 400px; color-scheme: light;"></iframe>
                             </div>
                         @endif
                     </div>
