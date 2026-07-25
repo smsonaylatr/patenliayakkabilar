86e70466c9c2fae785298deb9d63af7cd51df18b
Test User <test@test.com>
2026-07-25 07:22:55 +0300

    chore: GTM kodu GTM-583KKT3Q olarak güncellendi

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MTHFWWCB');</script>
+        })(window,document,'script','dataLayer','GTM-583KKT3Q');</script>
         <!-- End Google Tag Manager -->
 
         <!-- Google tag (gtag.js) -->
@@ -134,7 +134,7 @@
     </head>
     <body class="bg-brand-light text-brand-dark font-sans antialiased flex flex-col min-h-screen overflow-x-hidden">
         <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MTHFWWCB"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-583KKT3Q"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
         
