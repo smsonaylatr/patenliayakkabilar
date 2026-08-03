ee80beda8a0d1da0daeafd86f5bed394ac9abfa4
Test User <test@test.com>
2026-08-04 02:43:24 +0300

    fix: app.blade.php icinde yanlislikla silinen @endif etiketi geri eklendi (ParseError giderildi)

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -214,6 +214,8 @@
                                 @if(!empty($footerSettings['footer_youtube']))
                                 <a href="{{ $footerSettings['footer_youtube'] }}" aria-label="YouTube" target="_blank" class="text-gray-400 hover:text-brand-orange transition-colors">
                                     <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" clip-rule="evenodd" /></svg>
+                                </a>
+                                @endif
                             </div>
                         </div>
                         
