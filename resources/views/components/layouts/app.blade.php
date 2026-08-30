87a24b829c8a06985fb82a0359c947928307268f
Test User <test@test.com>
2026-08-31 00:33:22 +0300

    style: Mobil navbar ile Google mağaza kalitesi rozeti arasındaki çakışma düzeltildi

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -459,10 +459,18 @@
         @endif
 
         <!-- Google Customer Reviews -->
+        <style>
+            @media (max-width: 767px) {
+                #gcr-badge-container iframe {
+                    bottom: 75px !important;
+                }
+            }
+        </style>
         <script src="https://apis.google.com/js/platform.js?onload=renderGoogleGCR" async defer></script>
         <script>
           window.renderGoogleGCR = function() {
             var ratingBadgeContainer = document.createElement("div");
+            ratingBadgeContainer.id = "gcr-badge-container";
             document.body.appendChild(ratingBadgeContainer);
             window.gapi.load('ratingbadge', function() {
               window.gapi.ratingbadge.render(ratingBadgeContainer, {"merchant_id": 5828544730, "position": "BOTTOM_LEFT"});
