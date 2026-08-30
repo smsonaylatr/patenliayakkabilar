4ee4060928f6312d2fd7dc9ead7bf988cafc04f8
Test User <test@test.com>
2026-08-31 02:11:04 +0300

    fix: ProductsTable dosyasina use App\Models\Product importu eklendi

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Products\Tables;
 
+use App\Models\Product;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
