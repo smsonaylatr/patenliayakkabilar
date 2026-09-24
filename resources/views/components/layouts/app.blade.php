549886270ffc3c49b60c944668f667161fa014fa
Test User <test@test.com>
2026-09-25 02:20:32 +0300

    Mobil navbar: Sipariş ikonu Halikoy stiliyle uyumlu hale getirildi, etiket Siparişim olarak güncellendi

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -342,13 +342,13 @@
                         <span class="text-[11px] font-normal leading-none text-gray-800">Arama</span>
                     </button>
 
-                    {{-- 4. Sipariş Takip — Koli/paket ikonu --}}
+                    {{-- 4. Siparişim — Halikoy package icon --}}
                     <a href="/siparis-takip" wire:navigate class="flex flex-col items-center justify-center gap-[6px] text-gray-800 active:text-black transition-colors">
-                        <svg class="w-[24px] h-[24px]" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.2" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M20.5 7.278L12 12m0 0L3.5 7.278M12 12v9.5m8.5-5.278V7.778c0-.347 0-.52-.05-.672a1 1 0 00-.168-.29c-.1-.114-.24-.191-.521-.346l-7.25-3.993c-.258-.142-.387-.213-.524-.241a1 1 0 00-.374 0c-.137.028-.266.099-.524.241L3.839 6.47c-.28.155-.42.232-.52.346a1 1 0 00-.169.29c-.05.152-.05.325-.05.672v8.444c0 .347 0 .52.05.672a1 1 0 00.168.29c.1.114.24.191.521.346l7.25 3.993c.258.142.387.213.524.241a1 1 0 00.374 0c.137-.028.266-.099.524-.241l7.25-3.993c.28-.155.42-.232.52-.346a1 1 0 00.169-.29c.05-.152.05-.325.05-.672z"/>
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.25 9.639L7.75 4.917"/>
+                        <svg class="w-[24px] h-[24px]" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="1" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.0837 6.04167L10.0003 10M10.0003 10L2.91699 6.04167M10.0003 10V17.9167M17.5003 13.4308V6.56917C17.5003 6.28 17.5003 6.13542 17.4587 6.00583C17.4174 5.87646 17.3478 5.76042 17.2545 5.66583C17.1707 5.58083 17.0587 5.51708 16.8337 5.38917L10.467 1.76917C10.2518 1.64698 10.1441 1.58587 10.0295 1.56212C9.92843 1.5412 9.82401 1.5412 9.72293 1.56212C9.60837 1.58587 9.50066 1.64698 9.28535 1.76917L2.91869 5.38917C2.69372 5.51708 2.58162 5.58083 2.49787 5.66583C2.40445 5.76042 2.3349 5.87646 2.29357 6.00583C2.25195 6.13542 2.25195 6.28 2.25195 6.56917V13.4308C2.25195 13.72 2.25195 13.8646 2.29357 13.9942C2.3349 14.1235 2.40445 14.2396 2.49787 14.3342C2.58162 14.4192 2.69372 14.4829 2.91869 14.6108L9.28535 18.2308C9.50066 18.353 9.60837 18.4141 9.72293 18.4379C9.82401 18.4588 9.92843 18.4588 10.0295 18.4379C10.1441 18.4141 10.2518 18.353 10.467 18.2308L16.8337 14.6108C17.0587 14.4829 17.1707 14.4192 17.2545 14.3342C17.3478 14.2396 17.4174 14.1235 17.4587 13.9942C17.5003 13.8646 17.5003 13.72 17.5003 13.4308Z"/>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.75 8.02083L6.25 3.83333"/>
                         </svg>
-                        <span class="text-[11px] font-normal leading-none text-gray-800 whitespace-nowrap">Sipariş Takip</span>
+                        <span class="text-[11px] font-normal leading-none text-gray-800">Siparişim</span>
                     </a>
 
                     {{-- 5. Sepetim — Halikoy cart icon --}}
