08e6352d3abb588bcf43ff96edcc779893467da7
Test User <test@test.com>
2026-09-25 02:26:29 +0300

    Sepet: Beğenebilirsiniz bölümünü yatay carousel yaptı, sipariş onayla butonu görünür

--- resources/views/livewire/frontend/cart-drawer.blade.php
+++ resources/views/livewire/frontend/cart-drawer.blade.php
@@ -136,7 +136,7 @@
                                                 <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"/></svg>
                                             </div>
                                         </div>
-                                        <div class="flex gap-3 overflow-x-auto pb-2 -mx-5 px-5 lg:-mx-12 lg:px-12 snap-x snap-mandatory scrollbar-hide" style="scrollbar-width: none; -ms-overflow-style: none; -webkit-overflow-scrolling: touch;">
+                                        <div class="flex gap-3 overflow-x-auto pb-2 -mx-5 px-5 lg:-mx-12 lg:px-12 snap-x snap-mandatory hide-scrollbar" style="-webkit-overflow-scrolling: touch;">
                                             @foreach($recommendations as $rec)
                                                 <a href="{{ route('products.show', $rec->slug) }}" wire:navigate class="snap-start shrink-0 flex flex-col items-center rounded-xl border border-black/[0.06] p-3 hover:border-black/20 transition-colors group" style="width: 130px;">
                                                     <div class="rounded-lg overflow-hidden mb-2 relative" style="width:80px;height:80px;">
